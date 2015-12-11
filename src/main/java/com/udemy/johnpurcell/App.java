11f7c2452788d3e5c5ce8b934ec9aa47a8c33e3d
bogdans <bogdanskochynskyi@gmail.com>
2015-12-11 13:38:57 +0200

    [006] not works

--- src/main/java/com/udemy/johnpurcell/App.java
+++ src/main/java/com/udemy/johnpurcell/App.java
@@ -1,7 +1,7 @@
 package com.udemy.johnpurcell;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
@@ -11,10 +11,14 @@ public class App {
         Person person = new Person();
         person.speak();
 
-        //With Spring Beannjhj
-        ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+        //With Spring Bean
+//        ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("com\\udemy\\johnpurcell\\beans\\beans.xml"); //must be work in the same way
         Person person1 = (Person) context.getBean("person");
         person1.speak();
+        ((ClassPathXmlApplicationContext)context).close();
+
+
     }
 
 }
